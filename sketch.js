84b782d01909289f17ca353169f98cf74f408d42
thay1501 <thayane.souza15@escola.pr.gov.br>
2024-10-07 14:33:49 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(600, 600);
+    background(255)
+  }
+  function draw() {
+  
+   
+    stroke( "blue");
+    fill("red");
+    
+   // console.log(mouseIsPressed);
+    
+    
+  
+   if(mouseIsPressed) {
+    rect( mouseX , mouseY, 20, 35);
+  
+  
+    }
+  }
+  
